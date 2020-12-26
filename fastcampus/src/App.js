0f9985cf1e3895a315eebe6f0c66d92f80564129
dinomoon <ansrud45@gmail.com>
2020-12-26 17:39:31 +0900

    배열에 항목 수정하기

--- fastcampus/src/App.js
+++ fastcampus/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useRef} from 'react';
-import CreateUser from './14. 배열에 항목 제거하기/CreateUser';
-import UserList from './14. 배열에 항목 제거하기/UserList';
+import CreateUser from './15. 배열에 항목 수정하기/CreateUser';
+import UserList from './15. 배열에 항목 수정하기/UserList';
 import { useState } from 'react';
 
 function App() {
@@ -15,16 +15,19 @@ function App() {
       id: 1,
       username: 'mk',
       email: 'ansrud1003@naver.com',
+      active: true,
     },
     {
       id: 2,
       username: 'hj',
       email: 'heyjoo@google.com',
+      active: false,
     },
     {
       id: 3,
       username: 'fr',
       email: 'vhfl@google.com',
+      active: false,
     }
   ]);
 
@@ -59,6 +62,14 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id
+      ? {...user, active: !user.active}
+      : user
+    ))
+  }
+
   return (
     <>
       <CreateUser
@@ -67,7 +78,7 @@ function App() {
         onChange={onChange} 
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
